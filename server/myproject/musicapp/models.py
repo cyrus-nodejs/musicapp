ba7084c4d259464488fe4276ebc999baa8668ba3
king_cyrus <adeyemiemma45@gmail.com>
2025-06-16 21:48:00 +0100

    fifh commit

--- server/myproject/musicapp/models.py
+++ server/myproject/musicapp/models.py
@@ -1,7 +1,5 @@
 from django.db import models
-
 # Create your models here.
-
 from datetime import timezone
 from django.db import models
 import uuid
@@ -147,11 +145,12 @@ class Pricing(models.Model):
 class Order(models.Model):
     owner = models.ForeignKey(User, on_delete=models.CASCADE)
     pricing = models.ForeignKey(Pricing, related_name='pricing_order', on_delete=models.CASCADE)
+    uuid = models.UUIDField(default=uuid.uuid4, unique=True, editable=False)
     payment =  models.BooleanField(default=False)
     paymentid = models.CharField(max_length=100)
     bill = models.IntegerField()
     order_date = models.DateTimeField(auto_now=True)
-
+   
     def __str__(self):
         return self.pricing.plans
 
